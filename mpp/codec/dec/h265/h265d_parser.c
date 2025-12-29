fff87da91706d92913ba0254cee8c27eb093ae16
Herman Chen <herman.chen@rock-chips.com>
2025-12-29 14:03:38 +0800

    fix[license]: Restore the following files to LGPL
    mpp/codec/dec/av1/av1d_codec.h
    mpp/codec/dec/av1/av1d_cbs.c
    mpp/codec/dec/av1/av1d_cbs.h
    mpp/codec/dec/av1/av1d_parser2_syntax.c
    mpp/codec/dec/h265/h265d_codec.h
    mpp/codec/dec/h265/h265d_parser.c
    mpp/codec/dec/h265/h265d_ps.c
    mpp/codec/dec/vp9/vp9d_codec.h
    mpp/codec/dec/vp9/vp9d_parser.c
    mpp/codec/dec/vp9/vp9data.h
    mpp/codec/dec/vp9/vpx_rac.c
    mpp/codec/dec/vp9/vpx_rac.h
    
    Change-Id: I26bbefc82c7eda12d219b68ab680762da49c4035
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- mpp/codec/dec/h265/h265d_parser.c
+++ mpp/codec/dec/h265/h265d_parser.c
@@ -1,28 +1,20 @@
 /*
+ * Copyright (C) 2015 The FFmpeg project
+ * Copyright (c) 2015 Rockchip Electronics Co., Ltd.
  *
- * Copyright 2015 Rockchip Electronics Co. LTD
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * @file       h265d_parser.c
- * @brief
- * @author      csy(csy@rock-chips.com)
-
- * @version     1.0.0
- * @history
- *   2015.7.15 : Create
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #define MODULE_TAG "H265D_PARSER"
